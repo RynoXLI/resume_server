4b79a24d87d1526435e910d03cfccd1656c85400
Ryno_XLI <ryanfogle98@gmail.com>
2020-06-10 23:30:05 -0500

    Load site info from yaml file into flask template

--- app.py
+++ app.py
@@ -4,11 +4,19 @@ from datetime import datetime
 
 app = Flask(__name__)
 
+conf = yaml.load(open("config/site_config.yml", "r"), Loader=yaml.Loader)
+
 
 @app.route('/')
 def home():
     now = datetime.now()
     return render_template('index.html', time=now)
+
+
+@app.route('/new')
+def new_home():
+    now = datetime.now()
+    return render_template('base.html', time=now, conf=conf['index'])
 
 
 if __name__ == '__main__':
